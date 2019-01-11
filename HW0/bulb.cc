e93020df1e76f985520d3c289b71c636ee01924a
Logan Reed <42820084+Logan-Reed-39@users.noreply.github.com>
2019-01-11 09:34:43 -0700

    Add files via upload

---
+++ HW0/bulb.cc
@@ -0,0 +1,12 @@
+//Logan Reed
+//2017-01-20
+//CS253 HW0
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Two-One to get the weed and the other to call an electrician!\n";
+    return 0;
+}
